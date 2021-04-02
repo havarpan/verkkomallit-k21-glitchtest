e6395420f36962d0f918d8a1e23ba35fd010cec1
Harri Varpanen <harri.varpanen@jamk.fi>
2021-04-02 10:29:49 +0300

    hello

---
+++ lp-pohja/niemi_esim_3.3.1.py
@@ -0,0 +1,21 @@
+import pulp
+
+my_lp = pulp.LpProblem('testi', pulp.LpMaximize)
+ 
+x1 = pulp.LpVariable('x1', lowBound=0, cat='Continuous')
+x2 = pulp.LpVariable('x2', lowBound=0, cat='Continuous')
+x3 = pulp.LpVariable('x3', lowBound=0, cat='Continuous')
+x4 = pulp.LpVariable('x4', lowBound=0, cat='Continuous')
+x5 = pulp.LpVariable('x5', lowBound=0, cat='Continuous')
+
+# objective function
+my_lp += 0.1264*x1 + 0.107*x2 + 0.0764*x3 + 0.064*x4 + 0.0769*x5
+
+# constraints
+my_lp += x1 + x2 + x3 + x4 + x5 == 500000
+my_lp += x1 + x2 <= 250000
+my_lp += x4 + x5 >= 150000
+my_lp += 2*x1 + 8*x2 - 2*x3 - 5*x4 + x5 <= 0
+
+# solve
+my_lp.solve()
